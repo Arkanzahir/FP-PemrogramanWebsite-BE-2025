4cb5dbe47e72ac007db7f58fa7c54ea2001516c5
Arkanzahir <arkanzahir86@gmail.com>
2025-12-07 15:19:38 +0700

    [C1] fix: Remove unused play-count.schema.ts

--- src/api/game/game-list/pair-or-no-pair/schema/play-count.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const UpdatePlayCountSchema = z.object({
-  game_id: z.string().min(1, 'Game ID is required'),
-});
-
-export type IUpdatePlayCount = z.infer<typeof UpdatePlayCountSchema>;
